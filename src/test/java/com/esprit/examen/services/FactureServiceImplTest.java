90f84afe6ad901243202ae1aebe0e838b2a1c29c
MedMounibMadani <madani.mounib@gmail.com>
2023-03-22 19:52:21 +0100

    tests fixed

--- src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.junit4.SpringExtension;
-import com.esprit.examen.entities.Fature;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import com.esprit.examen.entities.Facture;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
